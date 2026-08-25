44c308189084ee256324671e1e43e030a60449ba
Tahn Rainbird <14843@holbertonstudents.com>
2026-08-25 00:54:15 +0000

    Added custom implementation of getline() and strtok()

---
+++ custom_funcs.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+
+char *_getline(char **input, size_t *size, FILE *stream)
+{
+	char *str = *input;
+	size_t chars;
+
+	if (!str)
+		return (NULL);
+
+	chars = read(fileno(stream), str, *size);
+
+	if (chars == -1)
+		return (NULL);
+
+	if (str[chars - 1] == '\n')
+		str[chars - 1] = '\0';
+
+	return (str);
+}
+
+char *_strtok(char *input, char *sep)
+{
+	int i = 0;
+	char *tok = malloc(64);
+	static char *copy;
+
+	if (input)
+		copy = strdup(input);
+	if (copy == NULL)
+		return (NULL);
+
+	while (copy[i] != '\0' && copy[i] != sep[0])
+	{
+		tok[i] = copy[i];
+		i++;
+	}
+
+	if (copy[i] == '\0')
+	{
+		copy = NULL;
+		return (tok);
+	}
+	
+	copy = copy + i + 1;
+	return (tok);
+}
