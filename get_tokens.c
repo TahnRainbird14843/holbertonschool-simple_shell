f4eb6112bd18675fd45d000c1a07a620a47ffd7b
14835-create <14835@holbertonstudents.com>
2026-08-25 02:59:08 +0000

    update

--- get_tokens.c
+++ get_tokens.c
@@ -4,8 +4,9 @@ char **get_tokens(char *input)
 {
 	char **tokens = malloc(sizeof(char *) * 64);
 	int i = 0;
+	char *token;
 
-	for (char *token = _strtok(input, " "); token; token = _strtok(NULL, " "))
+	for (token = _strtok(input, " "); token; token = _strtok(NULL, " "))
 		tokens[i++] = token;
 
 	tokens[i] = NULL;
