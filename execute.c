ff9ba901926c37f5a791233334f5c4864d0ab5d7
14835-create <14835@holbertonstudents.com>
2026-08-25 00:52:12 +0000

    execute and path

--- execute.c
+++ execute.c
@@ -1,11 +1,44 @@
 #include "shell.h"
 
+/**
+ * execute - command run from forking & calling execve
+ * @args: array tokens
+ *
+ * Return: 1 - shell running otherwise 0 - exit
+ */
+
 int execute(char **args)
 {
-	char *path;
-	pid_t pid;
+	char *full_path;
+	pid t pid;
 
 	if (!args[0])
 		return 1;
 
-	
+	if (strcmp(args[0], "exit") == 0)
+		return 0;
+
+	if (strcmp(args[0], "env") == 0)
+	{
+		for (char **e = enciron; *e; e++)
+			printf("%s\n", *e);
+		return 1;
+	}
+
+	full_path = get_path(args[0]);
+
+	pid = fork();
+
+	if (pid == 0)
+	{
+		if (full_path)
+			execve(full_path, args, environ);
+
+		printf("s: path not found\n", args[0]);
+		exit(1);
+	}
+
+	wait(NULL);
+	free(full_path);
+	return 1;
+}
---
+++ get_path.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * get_path - directory search in PATH for command
+ * @command: name
+ *
+ * Return: string with full path otherwise NULL
+ */
+
+char *get_path(char *command)
+{
+	char full_path[1024];
+	char *p_copy, *dire;
+	char *path = getenv("PATH");
+
+	if (!path)
+		return NULL;
+
+	p_copy = strdup(path);
+	if (!p_copy)
+		return NULL;
+
+	dire = strtok(p_copy, ":");
+
+	while (dire)
+	{
+		full_path[0] = '\0';
+		strcpy(full_path, dire);
+		strcat(full_path, "/");
+		strcat(full_path, command);
+
+		if (stat(full, &st) == 0)
+		{
+			free(p_copy);
+			return strdup(full_path);
+		}
+
+		dire = strtok(NULL, ":");
+	}
+
+	free(p_copy);
+
+	return NULL;
+}
