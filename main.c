49459427e1962b4e5e992e5ec073052003fdf11e
root <14835-create>
2026-08-27 02:32:37 +0000

    fixes and test

---
+++ main.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+int main(void)
+{
+	char *input, **args;
+	int r = 1;
+
+	while (r)
+	{
+		prompt();
+		input = input_read();
+		if(!input)
+			break;
+
+		args = get_tokens(input);
+		if (!args || !args[0])
+		{
+			free(input);
+			free(args);
+			continue;
+		}
+
+		check_exit(args, &r);
+		if (!r)
+		{
+			free(input);
+			free(args);
+			break;
+		}
+
+		execute(args);
+		free(input);
+		free(args);
+	}
+
+	return (0);
+}
