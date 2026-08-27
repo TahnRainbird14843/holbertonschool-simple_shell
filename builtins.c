54557fbc1455326c8c7069e4e6643008d2574913
root <14835-create>
2026-08-27 06:01:58 +0000

    betty things

--- builtins.c
+++ builtins.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * command_func - returns handler func for given command
+ * @cmd: command string
+ *
+ * Return: pointer to function executes command otherwise NULL
+ */
+
 void (*command_func(char *cmd))(char **, char **)
 {
 	cmd_t cmds[3] = {
@@ -19,11 +26,23 @@ void (*command_func(char *cmd))(char **, char **)
 	return (NULL);
 }
 
+/**
+ * exec_ls - executes ls command (execve())
+ * @args: argument vector command
+ * @env: environment variables
+ */
+
 void exec_ls(char **args, char **env)
 {
 	args[0] = "/bin/ls";
 	execve(args[0], args, env);
 }
+
+/**
+ * exec_env - executes env using execve()
+ * @args: argument vector command
+ * @env: environment variables
+ */
 
 void exec_env(char **args, char **env)
 {
