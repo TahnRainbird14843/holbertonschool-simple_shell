8b12bb90cbdf5ba43f7f73601853800521ccaa0b
Tahn Rainbird <14843@holbertonstudents.com>
2026-09-03 01:48:55 +0000

    implemented comments

--- main.c
+++ main.c
@@ -34,6 +34,14 @@ int main(__attribute__ ((unused)) int argc,
 			prompt();
 
 		input = input_read();
+		if (input[0] == '#' || input[0] == '\0')
+		{
+			free(input);
+			input = strdup("");
+		}
+		else
+			input = strtok(input, "#");
+
 		if (!input)
 		{
 			break;
